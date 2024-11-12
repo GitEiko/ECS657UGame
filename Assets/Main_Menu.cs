00f95fad0b5826688c0a75b366a15ae68da05a91
yagizefed <128502401+yagizefed@users.noreply.github.com>
2024-11-12 12:59:33 +0000

    MainMenu
    Main menu added to the game

---
+++ Assets/Main_Menu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void Quit_Game()
+    {
+        Application.Quit();  
+    }
+
+
+}
